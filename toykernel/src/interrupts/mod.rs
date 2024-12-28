bdb3401cb79d77aca9c1629d88acd0eb0a37fad3
Chris Koch <chrisko@google.com>
2024-12-28 02:35:37 +0000

    double fault handler
    Signed-off-by: Chris Koch <chrisko@google.com>

--- toykernel/src/interrupts/mod.rs
+++ toykernel/src/interrupts/mod.rs
@@ -6,6 +6,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -16,4 +17,14 @@ pub fn init_idt() {
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     serial_println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) -> ! {
+    panic!(
+        "EXCEPTION: DOUBLE FAULT ERROR {}\n{:#?}",
+        error_code, stack_frame
+    );
 }
